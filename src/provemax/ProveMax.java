07794a3ef3bbfac85631fc500279118d8b083ae8
Enzo Bulacio <72264564+ErCanaro@users.noreply.github.com>
2023-10-05 18:53:31 -0300

    DetalleCompraData Completo
    Se añadieron los métodos para la clase DetalleCompraData

--- src/provemax/ProveMax.java
+++ src/provemax/ProveMax.java
@@ -5,7 +5,9 @@
  */
 package provemax;
 
-import provemax.accesoADatos.ProductoData;
+import java.time.LocalDate;
+import java.util.Map;
+import provemax.accesoADatos.DetalleCompraData;
 import provemax.entidades.Producto;
 
 /**
@@ -21,10 +23,10 @@ public class ProveMax {
         //Proveedor p = new Proveedor("La Estrella", "Via Láctea", "03 03 456", true);
         
         //ProveedorData pvdData = new ProveedorData();
-        ProductoData prodData = new ProductoData();
-        for (Producto p : prodData.listaProductosConStockMenorAlMinimo()) {
-            System.out.println(p);
-        }
+        //ProductoData prodData = new ProductoData();
+        //for (Producto p : prodData.listaProductosConStockMenorAlMinimo()) {
+        //    System.out.println(p);
+        //}
         //CompraData compraData  =  new CompraData();
        //Producto producto = new Producto("Juguera", "Hace jugo", 12536.84, 18, true);
 //        System.out.println(compraData.buscarCompraPorId(1));
@@ -54,6 +56,19 @@ public class ProveMax {
         
         //System.out.println(compraData.buscarCompraPorId(1));
         
+        
+        
+        DetalleCompraData dcd = new DetalleCompraData();
+        
+//        for (Producto p : dcd.listaProductosPorFecha(LocalDate.of(2023,10, 1))){
+//            System.out.println(p);
+//        }
+        
+        for (Map.Entry<Producto, Integer> pv : dcd.listaProductosMasCompradoEntre(LocalDate.of(2023,10,1), LocalDate.of(2023,10,6)).entrySet()){
+             System.out.println(pv.getKey().getNombre() + "  =  " + pv.getValue());
+        }
+
     }
+    
     
 }
--- src/provemax/accesoADatos/DetalleCompraData.java
+++ src/provemax/accesoADatos/DetalleCompraData.java
@@ -2,17 +2,26 @@
 package provemax.accesoADatos;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import javax.swing.JOptionPane;
 import provemax.entidades.DetalleCompra;
+import provemax.entidades.Producto;
+import provemax.entidades.Proveedor;
 
 
 public class DetalleCompraData {
     private Connection con = null;
     private CompraData compraData = new CompraData();
     private ProductoData prodData = new ProductoData();
+    private ProveedorData provData = new ProveedorData();
 
     public DetalleCompraData() {
         con =  Conexion.getConexion();
@@ -75,8 +84,7 @@ public class DetalleCompraData {
             ps.setDouble(2, detalle.getPrecioCosto());
             ps.setInt(3, detalle.getCompra().getIdCompra());
             ps.setInt(4, detalle.getProducto().getIdProducto());
-            
-            
+
             int modificado = ps.executeUpdate();
                                    
             if (modificado == 1) {
@@ -91,11 +99,154 @@ public class DetalleCompraData {
     }
     
     //Buscar detalle por 1d
-    //Lista Productos y Fecha
+    public DetalleCompra buscarDetallePorId(int idDetalle) {
+        String sql = "SELECT * from detalleCompra WHERE idDetalle = ?";
+        DetalleCompra detalleCompra = new DetalleCompra();
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idDetalle);
+            
+            ResultSet rs = ps.executeQuery();
+                                   
+            if (rs.next()) {
+                detalleCompra.setIdDetalle(rs.getInt("idDetalle"));
+                detalleCompra.setCantidad(rs.getInt("cantidad"));
+                detalleCompra.setPrecioCosto(rs.getDouble("precioCosto"));
+                detalleCompra.setProducto(prodData.buscarProductoPorId(rs.getInt(rs.getInt("idProducto"))));
+                detalleCompra.setCompra(compraData.buscarCompraPorId(rs.getInt("idCompra")));
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la tabla DetalleCompra");
+        }
+        return detalleCompra;
+    }
+    //Lista de Productos x Fecha
+    public List<Producto> listaProductosPorFecha(LocalDate fecha) {
+        ArrayList<Producto> listaProductos = new ArrayList<>();
+        
+        String sql = "SELECT * FROM detallecompra dc JOIN compra ON dc.idCompra=compra.idCompra WHERE fecha = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setDate(1, Date.valueOf(fecha));
+            
+            ResultSet rs = ps.executeQuery();
+                                   
+            while (rs.next()) {
+                Producto producto = prodData.buscarProductoPorId((rs.getInt("idProducto")));
+                
+                listaProductos.add(producto);
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la tabla DetalleCompra");
+        }
+        return listaProductos;
+    }
     //Lista Productos por compra
-    //Lista Proveedores por producto
-    //Lista Productos mas comprados entre fechas
+    public List<Producto> listaProductosPorCompra(int idCompra) {
+        ArrayList<Producto> listaProductos = new ArrayList<>();
+        
+        String sql = "SELECT * FROM detallecompra WHERE idCompra = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idCompra);
+            
+            ResultSet rs = ps.executeQuery();
+                                   
+            if (rs.next()) {
+                Producto producto = prodData.buscarProductoPorId(rs.getInt("idProducto"));
+                
+                listaProductos.add(producto);
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la tabla DetalleCompra");
+        }
+        return listaProductos;
+    }
+    //Lista productos por Proveedor
+    public List<Producto> listaProductosPorProveedor(int idProveedor) {
+        ArrayList<Producto> listaProductos = new ArrayList<>();
+        
+        String sql = "SELECT * FROM detallecompra dc JOIN compra ON dc.idCompra=compra.idCompra WHERE idProveedor = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idProveedor);
+            
+            ResultSet rs = ps.executeQuery();
+                                   
+            if (rs.next()) {
+                Producto producto = prodData.buscarProductoPorId(rs.getInt("idProducto"));
+                
+                listaProductos.add(producto);
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la tabla DetalleCompra");
+        }
+        return listaProductos;
+    }
     
+    //Lista Proveedores por producto
+    public List<Proveedor> listaProveedoresPorProducto(int idProducto) {
+        ArrayList<Proveedor> listaProveedores = new ArrayList<>();
+        
+        String sql = "SELECT * FROM detallecompra dc JOIN compra ON dc.idCompra=compra.idCompra WHERE idProducto = ?";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idProducto);
+            
+            ResultSet rs = ps.executeQuery();
+                                   
+            if (rs.next()) {
+                Proveedor proveedor = provData.buscarProveedorPorId(rs.getInt("idProveedor"));
+                
+                listaProveedores.add(proveedor);
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la tabla DetalleCompra");
+        }
+        return listaProveedores;
+    }
+    
+    //Lista Productos mas comprados entre fechas
+     public Map<Producto, Integer> listaProductosMasCompradoEntre(LocalDate fechaInicio, LocalDate fechaFin) {
+        Map<Producto, Integer> listaProductos = new LinkedHashMap<>();
+        
+        String sql = "SELECT *, SUM(cantidad) as total FROM detallecompra dc JOIN compra ON dc.idCompra=compra.idCompra "
+                + "WHERE fecha >= ? AND fecha <= ? GROUP BY idProducto ORDER BY total DESC";
+        
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setDate(1, Date.valueOf(fechaInicio));
+            ps.setDate(2, Date.valueOf(fechaFin));
+            
+            ResultSet rs = ps.executeQuery();
+                                   
+            while (rs.next()) {
+                Producto producto = prodData.buscarProductoPorId(rs.getInt("idProducto"));
+                int cant = rs.getInt("total");
+                listaProductos.put(producto, cant);
+            }
+            ps.close();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al conectar con la tabla DetalleCompra");
+        }
+        return listaProductos;
+    }
     
     
 }
